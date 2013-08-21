e54b89cf0eec4c311a84ce8f154bb6c52aecb965
nakamura shinichirou <naka5313@gmail.com>
2013-08-21 12:47:16 +0900

    add api method

--- lib/dog_weather/weather.rb
+++ lib/dog_weather/weather.rb
@@ -2,9 +2,19 @@ require 'net/http'
 
 module DogWeather
   class Weather
+    attr_reader :url
+
+    def initialize
+      @url = "api.openweathermap.org"
+    end
+
     def weather(lat, lon)
-      url = "api.openweathermap.org"
-      res = Net::HTTP.get(url, "/data/2.5/weather?lat=#{lat}&lon=#{lon}")
+      res = Net::HTTP.get(@url, "/data/2.5/weather?lat=#{lat}&lon=#{lon}")
+      puts res 
+    end
+
+    def city(city_name)
+      res = Net::HTTP.get(@url, "/data/2.5/weather?q=#{city_name}")
       puts res 
     end
   end
